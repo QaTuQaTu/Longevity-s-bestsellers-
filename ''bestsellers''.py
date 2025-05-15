66e6d79e11f56d1c5855ae982c9c845aaf6ce889
Tuqa <tuqamalkhatib@gmail.com>
2025-05-15 19:50:51 +0300

    Add files via upload

---
+++ ''bestsellers''.py
@@ -0,0 +1,9 @@
+answer = input('Do you wamt to explore our top sellers?')
+if answer=='yes':
+    inter=input('Category of intrest:')
+    if inter=='food':
+        print("Milk 1l, Raisin cookies, Peaches")
+    else:
+        print("Laundry detergent, Shoe brush")
+else:
+    print("Let us know if you change your mind!")
